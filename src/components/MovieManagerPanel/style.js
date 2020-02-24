3d7e085417de1f26cd6587b9e19adf30f106eb16
Alejandro <mapina.tamayo@gmail.com>
2020-02-24 15:05:23 -0500

    stylizing the main component

---
+++ src/components/MovieManagerPanel/style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+/************************************************************
+ ****** Style for MovieManagerPanel *************************
+ ************************************************************/
+export const ContainerMovieManager = styled.div`
+  width: 700px;
+  height: 600px;
+  padding: 0.5em;
+  @media(max-width:40rem){
+    width: 98%;
+    /*padding: 2em;*/
+  }
+`;
+/************************************************************
+ ****** End Style for MovieManagerPanel *********************
+ ************************************************************/
