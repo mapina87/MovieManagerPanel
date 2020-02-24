4f5bad947b0ba86f74431a9dab662548efde4d7a
Alejandro <mapina.tamayo@gmail.com>
2020-02-24 15:08:07 -0500

    stylizing the form

--- src/components/MovieManagerPanel/style.js
+++ src/components/MovieManagerPanel/style.js
@@ -62,4 +62,72 @@ export const ContainerSearch = styled.div`
  ****** End Styles of SearchPanel and their components **********
  ****************************************************************/
 
- 
+ /************************************************************
+ ****** Styles of FormPanel  and their components ***********
+ ************************************************************/
+//Style for form panel.
+export const Form = styled.form`
+  border: 1px solid black;
+  padding: 7px 0px;
+  margin-top: 14px;
+  border-radius: 0.4em;
+  width: 100%;
+  align-items: center;
+  display:flex;
+  justify-content:space-between;
+ @media(max-width:40rem){
+    width: 83%;
+    flex-direction:column;
+    margin:1em 0em 0em 1.3em;
+  }
+`;
+
+//Style for field of name.
+export const NameData = styled(SearchName)`
+  border-radius: 0.5em;
+  width: 33%;
+  height: 35px;
+  margin-left: 5px;
+  @media(max-width:40rem){
+    width: 80%;
+    margin-top:0.8em;
+    margin-right:0.1em;
+  }
+`;
+
+//Style for field of genre.
+export const GenreData = styled(NameData)`
+  margin-right: 4rem;
+  @media(max-width:40rem){
+    width: 80%;
+    margin-top:0.8em;
+    margin-right:0.1em;
+  }
+`;
+
+//Style for button to add movie.
+export const Button = styled.button`
+  border-radius: 0.5em;
+  width: 40px;
+  height: 40px;
+  margin-right: 1rem;
+  @media(max-width:40rem){
+    display:none;
+  }
+`;
+
+export const ButtonMovil = styled.button`
+    width:100%;
+    height: 40px;
+    border-radius: 0.5em;
+    display:none;
+    margin-top:5px;
+    @media(max-width:40rem){
+      display:block;
+      width: 84%;
+      margin:1.2em 0.2em 1.2em 0em;
+  }
+`
+/************************************************************
+ ****** End Styles of FormPanel  and their components *******
+ ************************************************************/
